a9912bfb24ad21fdfd3bd4a9f0d757e4116ffae1
Mararok <mararok@gmail.com>
2019-12-10 09:51:52 +0100

    fix logger formatter (#39)

--- src/SAREhub/Microt/Logger/RequestIdProcessor.php
+++ src/SAREhub/Microt/Logger/RequestIdProcessor.php
@@ -4,16 +4,19 @@
 namespace SAREhub\Microt\Logger;
 
 
-class RequestIdProcessor {
-	
-	private $requestId;
-	
-	public function __construct(string $requestId) {
-		$this->requestId = $requestId;
-	}
-	
-	public function __invoke(array $record): array {
-		$record['extra']['requestId'] = $this->requestId;
-		return $record;
-	}
+class RequestIdProcessor
+{
+
+    private $requestId;
+
+    public function __construct(string $requestId)
+    {
+        $this->requestId = $requestId;
+    }
+
+    public function __invoke(array $record): array
+    {
+        $record['extra']['requestId'] = $this->requestId;
+        return $record;
+    }
 }
--- src/SAREhub/Microt/Logger/StandardLogFormatter.php
+++ src/SAREhub/Microt/Logger/StandardLogFormatter.php
@@ -5,14 +5,15 @@ namespace SAREhub\Microt\Logger;
 
 use Monolog\Formatter\JsonFormatter;
 
-class StandardLogFormatter extends JsonFormatter {
-	
-	protected function normalize($data) {
-		
-		if ($data instanceof \DateTime) {
-			return $data->format(DATE_ATOM);
-		}
-		
-		return parent::normalize($data);
-	}
+class StandardLogFormatter extends JsonFormatter
+{
+
+    protected function normalize($data, $depth = 0)
+    {
+        if ($data instanceof \DateTime) {
+            return $data->format(DATE_ATOM);
+        }
+
+        return parent::normalize($data);
+    }
 }
